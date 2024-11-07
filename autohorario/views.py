22308907357e518682558ac3a18297ce583511d3
pedro.southier <phsouthier@hotmail.com>
2024-11-07 20:37:30 -0300

    add login

--- autohorario/views.py
+++ autohorario/views.py
@@ -1,9 +1,20 @@
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.backends import UserModel
 from django.shortcuts import render
+from autohorario.forms import FormLogin
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.utils import timezone
 
 def index(request):
-    nome = "Gusvn "
-    return HttpResponse(f"Olár {nome}")
+    if request.user.is_authenticated:
+        nome = "Gusvn "
+        return HttpResponse(f"Olár {nome}")
+        # return render(request, 'certificados/index.html', contexto)
+    else:
+        return HttpResponseRedirect("/autohorario/login/")
+
 
 def testecomponents(request):
     return render(request, "components/menu.html")
@@ -11,8 +22,29 @@ def testecomponents(request):
 def testecomponents2(request):
     return render(request, "components/cards.html")
 
-def base(request):
-    return render(request, "base.html")
+def fazer_login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/autohorario/")
+
+    if request.method == 'POST':
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data['usuario']
+            senha = form.cleaned_data['senha']
+            user = authenticate(request, username=usuario, password=senha) # retorna None se o usuário não é válido
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect("/autohorario/")
+            else:
+                messages.error(request, "Usuário e/ou senha incorretos. Tente de novo.")
+        else:
+            messages.error(request, "Preencha corretamente o formulário")
+
+    else:
+        form = FormLogin()
+
+    contexto = {"form": form}
+    return render(request, 'login.html', contexto)
 
 def recoverPassword(request):
     return render(request, "password-recovery.html")
